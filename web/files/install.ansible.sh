ee8ab5176619608819f18049995b97fa3e59c816
Johnathan Bangura <135305037+jbangurajr@users.noreply.github.com>
2024-06-29 01:02:55 -0400

    Update Vagrant configuration to use the "ubuntu/kinetic64" box

--- web/files/install.ansible.sh
+++ web/files/install.ansible.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -x
 
 sudo apt-get update
-pip install --break-system-packages --user ansible
-sudo apt install pipx -y
 sudo apt-get install -y python3-pip
-sudo apt upgrade -y
-sudo apt install software-properties-common
+pip install --break-system-packages --user ansible
 sudo apt install ansible-core -y
 
 # Only LTS releases install ansible automaitcally. For other releases, you need to install ansible manually.
