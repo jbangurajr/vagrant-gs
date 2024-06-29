107d1e17d23b4317e3d360903ae0ba35851b3c03
Johnathan Bangura <135305037+jbangurajr@users.noreply.github.com>
2024-06-29 00:26:37 -0400

    Update Vagrant configuration to use the "generic-x64/ubuntu2310" box

--- web/files/install.ansible.sh
+++ web/files/install.ansible.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
-sudo apt-get update
-sudo apt-get install -y python3-pip
-pip install ansible
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible 
 
 # Only LTS releases install ansible automaitcally. For other releases, you need to install ansible manually.
